848319db096ed9a39fcc0a244b7095c1645586e9
Dinith Minura <dinith.m@eyepax.com>
2020-01-18 21:54:41 +0530

    Initial Commit

---
+++ TimeOverlayVideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeOverlayVideoPlayer
+//
+//  Created by Malsha Hansini on 1/18/20.
+//  Copyright © 2020 eyepax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
